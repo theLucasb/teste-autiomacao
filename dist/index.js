7198f656ea4b37d429a9ef5c3e10346e4f60558d
Lucas Rodrigo <lucasrodrigodes@gmail.com>
2021-12-22 10:15:56 -0300

    add new

--- dist/index.js
+++ dist/index.js
@@ -17758,7 +17758,7 @@ async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
     const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
 
-    const randomPos = Math.round(Math.random() * 1000);
+    const randomPos = Math.round(Math.random() * 10);
     const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
     await axios.get(url).then(function (response) { 
         var x = response.data.results
